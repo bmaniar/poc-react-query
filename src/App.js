93ed2a74f74897912ecfdb470c5f6e5dd7d0c504
Bhargav Maniar <bhargav.maniar@bookmyshow.com>
2021-10-16 23:23:26 +0530

    Adding router and posts homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+
 import './App.css';
 
 function App() {
+  // Create a client
+  const queryClient = new QueryClient()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
+  )
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React from 'react'
+// import { NavLink } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const getPosts = async () => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    console.log(data)
+    return data
+}
+
+function Home() {
+    const { data, error, isError, isLoading, isFetching } = useQuery('posts', getPosts)
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (isError) {
+        return <div>Error! {error.message}</div>
+    }
+
+    return (
+        <div className='container'>
+            <h1>Posts</h1>
+            { data &&
+                <ul>
+                    {
+                        data
+                            .slice(0, 20)
+                            .map((post) => (
+                                <li key={`post-${post.id}`}>
+                                    {post.title}
+                                </li>
+                            ))
+                    }
+                </ul>
+            }
+            { isFetching && <p>updating...</p>}
+        </div>
+    );
+}
+
+export default Home
